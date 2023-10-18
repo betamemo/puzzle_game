617ab55a0e804718e90c5df2c4e9d50b7253ccfd
BK <betamemo@BK.local>
2023-10-18 12:07:35 +0700

    edit screen to stop animation

--- board.py
+++ board.py
@@ -12,6 +12,7 @@ class Board():
         self.turtle = Turtle()
         self.turtle.penup()
         self.turtle.hideturtle()
+        # self.turtle.goto(-300, 100)
         self.tiles = [
             [1, 2, 3, 4],
             [5, 6, 7, 8],
@@ -21,13 +22,9 @@ class Board():
         self.draw_tiles()
 
     def draw_tiles(self):
-        x = 0
-        y = 0
-        add = 50
+        space = 50
         for row in self.tiles:
             for col in row:
-                self.turtle.goto(x, y)
                 self.turtle.write(f'{col}', align='center', font=FONT)
-                x += add
-            x = 0
-            y -= add
+                self.turtle.goto(self.turtle.xcor() + space, self.turtle.ycor())
+            self.turtle.goto(0, self.turtle.ycor() - space)
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from turtle import Screen
 from board import Board
 
 screen = Screen()
+screen.setup(500, 500)
+screen.tracer(0)
 
 board = Board()
+
+screen.update()
 
 screen.exitonclick()
